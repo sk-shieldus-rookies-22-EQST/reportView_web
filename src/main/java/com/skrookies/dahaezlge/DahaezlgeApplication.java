b70904c0dd68c1596fc27b1120815f6d48740db3
heavenring <anstn0104@naver.com>
2025-01-21 09:46:48 +0900

    first

---
+++ src/main/java/com/skrookies/dahaezlge/DahaezlgeApplication.java
@@ -0,0 +1,13 @@
+package com.skrookies.dahaezlge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DahaezlgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DahaezlgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skrookies/dahaezlge/DahaezlgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skrookies.dahaezlge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DahaezlgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
