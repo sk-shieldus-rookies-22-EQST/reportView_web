34cd6efe117add5b0b95ad8cb72a7891bb422ef5
heavenring <anstn0104@naver.com>
2025-01-21 17:47:21 +0900

    setting: Tomcat 버전 최신으로 변경 및 임시 db 연결

--- src/main/java/com/skrookies/dahaezlge/DahaezlgeApplication.java
+++ src/main/java/com/skrookies/dahaezlge/DahaezlgeApplication.java
@@ -1,13 +1,17 @@
 package com.skrookies.dahaezlge;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DahaezlgeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DahaezlgeApplication.class, args);
+        new SpringApplicationBuilder(DahaezlgeApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
     }
 
 }
