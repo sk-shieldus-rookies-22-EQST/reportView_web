c10f94aae9de8c66bd253fed9c350e4547469ce1
dongjin <doshim1234@naver.com>
2025-01-22 21:35:03 +0900

    qnaList 수정

--- src/main/java/com/skrookies/dahaezlge/controller/qna/QnaController.java
+++ src/main/java/com/skrookies/dahaezlge/controller/qna/QnaController.java
@@ -8,12 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/qnaList")
 @Slf4j
 public class QnaController {
     private final QnaService QnaService;
@@ -25,7 +23,6 @@ public class QnaController {
         return "qnaWrite";
     }
 
-    @GetMapping("/qnaWrite")
 
     @PostMapping("/qnaWriteProcess")
     public String qnaWrite(@ModelAttribute QnaDto QnaDto) {
