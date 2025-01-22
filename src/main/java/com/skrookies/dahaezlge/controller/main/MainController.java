5ea8c58755d9bbbf083b3a01320f305f5f464975
syng <qkrtjs9321@gmail.com>
2025-01-22 17:26:10 +0900

    test-test

--- src/main/java/com/skrookies/dahaezlge/controller/main/MainController.java
+++ src/main/java/com/skrookies/dahaezlge/controller/main/MainController.java
@@ -1,15 +1,23 @@
 package com.skrookies.dahaezlge.controller.main;
 
-import ch.qos.logback.core.model.Model;
+import com.skrookies.dahaezlge.controller.user.Dto.UserDto;
+import com.skrookies.dahaezlge.service.user.UserService;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final UserService userService;
 
     @GetMapping("/index")
     public String main_form(){
@@ -47,9 +55,28 @@ public class MainController {
     }
 
     @GetMapping("/myInfo")
-    public String myInfo_form(){
+    public String myInfo_form(Model model, HttpSession session){
 
         log.info("page_move: myInfo.jsp");
+
+        String user_id = (String) session.getAttribute("id");
+        if (user_id == null) {
+            log.warn("User not logged in");
+            return "redirect:/loginForm";  // 로그인 페이지로 리다이렉트
+        }
+
+        // 사용자 정보를 가져오기
+        List user_info = userService.userInfo(user_id);
+        if (user_info == null || user_info.isEmpty()) {
+            log.error("User information not found for user_id: " + user_id);
+            return "redirect:/loginForm";  // 예시로 에러 페이지로 리다이렉트
+        }
+
+        // UserDto 객체 생성
+        //UserDto userDto = new UserDto(user_id, (String)user_info.get(0));
+
+        // 모델에 데이터 추가
+        model.addAttribute("user_id", new UserDto(user_id, (String)user_info.get(0)));
         return "myInfo";
     }
 
