8a1a2a593290fed7bb2fd36e89e5e0fb3bff1808
heavenring <anstn0104@naver.com>
2025-01-22 16:08:22 +0900

    Entity 추가: UserPoint

--- src/main/java/com/skrookies/dahaezlge/entity/userPoint/UserPoint.java
+++ src/main/java/com/skrookies/dahaezlge/entity/userPoint/UserPoint.java
@@ -1,4 +1,30 @@
 package com.skrookies.dahaezlge.entity.userPoint;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserPoint {
+
+    @Id
+    @Column(name = "point_user_id")
+    private String pointUserId;
+
+    @Column(name = "point")
+    private Integer point;
+
+
+    public UserPoint(String pointUserId, Integer point) {
+        this.pointUserId = pointUserId;
+        this.point = point;
+    }
+
 }
