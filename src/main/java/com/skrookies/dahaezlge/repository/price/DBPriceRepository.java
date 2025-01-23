626533f7d60bf0b1df0ff9b7e67ecefeffe6fec2
dongjin <doshim1234@naver.com>
2025-01-23 18:21:27 +0900

    qnaList.jsp 정상 작동

--- src/main/java/com/skrookies/dahaezlge/repository/price/DBPriceRepository.java
+++ src/main/java/com/skrookies/dahaezlge/repository/price/DBPriceRepository.java
@@ -1,6 +1,5 @@
 package com.skrookies.dahaezlge.repository.price;
 
-import com.skrookies.dahaezlge.entity.cart.CartId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
