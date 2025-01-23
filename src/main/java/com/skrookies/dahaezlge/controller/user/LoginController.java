bcd0e18dab6a3c37ef047dc8e9950e7566fb5c62
syng <qkrtjs9321@gmail.com>
2025-01-23 10:38:08 +0900

    LoginController, banner: login_session_success

--- src/main/java/com/skrookies/dahaezlge/controller/user/LoginController.java
+++ src/main/java/com/skrookies/dahaezlge/controller/user/LoginController.java
@@ -33,20 +33,17 @@ public class LoginController {
 
         if(userService.login(loginDto.getUser_id(), loginDto.getUser_pw())){
             log.info("user_id = " + user_id);
-            model.addAttribute("user_id",user_id);
-            SessionDto sessionDto = new SessionDto(loginDto.getUser_id());
 
-            session.setAttribute("id", sessionDto);
-            log.info("sessionId = " + session.getAttribute("id"));
+            session.setAttribute("user_id", user_id);
+            log.info("sessionId = " + session.getAttribute("user_id"));
 
             /** point select 메소드 */
             int point = userService.userPoint(user_id);
+            session.setAttribute("point", point);
             log.info("point = " + point);
 
-            /** Point model로 전달 */
-            model.addAttribute("point", point);
             log.info("user_id = " + user_id);
-            return "/index";
+            return "redirect:/index";
         }
         else{
             log.info("없는 아이디");
@@ -58,8 +55,8 @@ public class LoginController {
 
     @GetMapping("/logout")
     public String logout(Model model, HttpSession session) {
-        session.setAttribute("id", null);
-        return "index";
+        session.removeAttribute("user_id");
+        return "redirect:/index";
     }
 
 }
