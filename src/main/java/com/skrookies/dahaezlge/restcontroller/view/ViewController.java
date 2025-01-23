3ec48464d4afa7676fdc7f86da7feb3168966971
heavenring <anstn0104@naver.com>
2025-01-23 15:57:28 +0900

    rest api: view/booklist 추가

--- src/main/java/com/skrookies/dahaezlge/restcontroller/view/ViewController.java
+++ src/main/java/com/skrookies/dahaezlge/restcontroller/view/ViewController.java
@@ -1,6 +1,8 @@
 package com.skrookies.dahaezlge.restcontroller.view;
 
+import com.skrookies.dahaezlge.controller.book.Dto.BookDto;
 import com.skrookies.dahaezlge.restcontroller.util.StatusDto;
+import com.skrookies.dahaezlge.restcontroller.view.dto.BookListDto;
 import com.skrookies.dahaezlge.restcontroller.view.dto.ViewerDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +13,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/view")
 @RequiredArgsConstructor
 public class ViewController {
+
+
+    @PostMapping("/booklist")
+    public ResponseEntity<List<BookListDto>> bookList(){
+
+        /** bookService에서 Book Entity를 모두 가져온다
+         * BookListDto 형식에 맞게 데이터를 setting한다.
+         * 만든 BookListDto를 List<BookListDto>에 add 한다.*/
+
+        List<BookListDto> bookListDto = new ArrayList<>();
+        for(int i = 0; i < 100; i++) {
+
+            bookListDto.add(null);
+        }
+
+        return ResponseEntity.ok()
+                .body(bookListDto);
+
+    }
 
 
 
