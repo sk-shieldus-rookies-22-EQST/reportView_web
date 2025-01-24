568583f57caf5c32445030bc00e325989e44eee5
heavenring <anstn0104@naver.com>
2025-01-24 17:28:33 +0900

    Make: insertUserPoint Interface

--- src/main/java/com/skrookies/dahaezlge/repository/userPoint/UserPointRepository.java
+++ src/main/java/com/skrookies/dahaezlge/repository/userPoint/UserPointRepository.java
@@ -6,4 +6,9 @@ public interface UserPointRepository {
      * user_id를 통해 유저가 보유한 포인트 반환
      * @return List<int> */
     int selectUserPoint(String user_id);
+
+    /** 유저 포인트 할당
+     * user_id를 통해 유저가 회원가입 했을 때 포인트 할당
+     * @return Boolean */
+    Boolean insertUserPoint(String user_id);
 }
