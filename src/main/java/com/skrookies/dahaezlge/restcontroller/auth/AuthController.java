5d31fdc1b906241470e1a3760d76d5288b1aa074
heavenring <anstn0104@naver.com>
2025-02-03 00:17:34 +0900

    fix: json 형식 미 반환 문제 해결

--- src/main/java/com/skrookies/dahaezlge/restcontroller/auth/AuthController.java
+++ src/main/java/com/skrookies/dahaezlge/restcontroller/auth/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/auth")
+@RequestMapping(value = "/auth", produces = "application/json")
 @RequiredArgsConstructor
 public class AuthController {
 
--- src/main/java/com/skrookies/dahaezlge/restcontroller/user/dto/UserPointDto.java
+++ src/main/java/com/skrookies/dahaezlge/restcontroller/user/dto/UserPointDto.java
@@ -1,10 +1,14 @@
 package com.skrookies.dahaezlge.restcontroller.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserPointDto {
 
     private int user_point;
