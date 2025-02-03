e1c2eb67932888833963ffc0551cf129f37a6bd0
syng <qkrtjs9321@gmail.com>
2025-02-03 11:30:07 +0900

    fix : Main - use Grid

--- src/main/java/com/skrookies/dahaezlge/repository/book/BookRepository.java
+++ src/main/java/com/skrookies/dahaezlge/repository/book/BookRepository.java
@@ -1,7 +1,6 @@
 package com.skrookies.dahaezlge.repository.book;
 
 import com.skrookies.dahaezlge.controller.book.Dto.BookDto;
-import com.skrookies.dahaezlge.entity.book.Book;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,16 +13,16 @@ public interface BookRepository {
     List<BookDto> getCartBookInfo(List<Long> bookIdList);
 
     /** 페이지별로 책 리스트 가져오기 */
-    List<Map<String, Object>> getBooks(int page, int pageSize);
+    List<Map<String, Object>> getBooks();
 
     /** 검색어 별 책 리스트 가져오기 */
-    List<Map<String, Object>> getBooksWithKeyword(String keyword, int page, int pageSize);
+    List<Map<String, Object>> getBooksWithKeyword(String keyword);
 
     /** 게시 날짜 별 책 리스트 가져오기 */
-    List<Map<String, Object>> getBooksWithDate(LocalDateTime sdate, LocalDateTime edate, int page, int pageSize);
+    List<Map<String, Object>> getBooksWithDate(LocalDateTime sdate, LocalDateTime edate);
 
     /** 검색어 및 게시 날짜 별 책 리스트 가져오기 */
-    List<Map<String, Object>> getBooksWithBoth(String keyword, LocalDateTime sdate, LocalDateTime edate, int page, int pageSize);
+    List<Map<String, Object>> getBooksWithBoth(String keyword, LocalDateTime sdate, LocalDateTime edate);
 
 
     // 전체 책 개수
