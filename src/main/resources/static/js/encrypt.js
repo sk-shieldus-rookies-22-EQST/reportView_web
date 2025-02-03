0d2cd1a93060f746b41ac510dbb9f84d478f4899
e0-ofni <e0.ofni@gmail.com>
2025-02-03 21:57:43 +0900

    edit parameter id,name

--- src/main/resources/static/js/encrypt.js
+++ src/main/resources/static/js/encrypt.js
@@ -1,18 +1,8 @@
 async function encryptAES(key, iv, data) {
-    // Base64로 디코딩하고 16바이트로 패딩하는 함수
-    function padTo16Bytes(inputBytes) {
-        let padded = new Uint8Array(16);
-        padded.set(inputBytes.slice(0, 16));  // 최대 16바이트까지 복사
-        return padded;
-    }
-
     // Base64로 디코딩된 키와 IV를 16바이트로 패딩
     let keyBytes = Uint8Array.from(atob(key), c => c.charCodeAt(0));
     let ivBytes = Uint8Array.from(atob(iv), c => c.charCodeAt(0));
 
-    // 16바이트로 패딩
-    keyBytes = padTo16Bytes(keyBytes);  // 키 패딩
-    ivBytes = padTo16Bytes(ivBytes);    // IV 패딩
 
     // AES 키 가져오기
     const cryptoKey = await crypto.subtle.importKey(
