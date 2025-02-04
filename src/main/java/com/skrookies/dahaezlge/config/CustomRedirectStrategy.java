76ec922b7caa6c1f54982fa9f055c69e0801d284
heavenring <anstn0104@naver.com>
2025-02-04 11:17:36 +0900

    fix: Spring Security 로그인, 로그아웃 관련 오류 해결 및 csrf 코드 수정

---
+++ src/main/java/com/skrookies/dahaezlge/config/CustomRedirectStrategy.java
@@ -0,0 +1,16 @@
+package com.skrookies.dahaezlge.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.RedirectStrategy;
+
+import java.io.IOException;
+
+public class CustomRedirectStrategy implements RedirectStrategy {
+
+    @Override
+    public void sendRedirect(HttpServletRequest request, HttpServletResponse response, String url) throws IOException {
+        // 리디렉트 횟수 제한 없이 무조건 수행
+        response.sendRedirect(url);
+    }
+}
