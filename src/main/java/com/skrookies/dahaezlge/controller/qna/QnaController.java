d2d7ab647250e432fba1c66b10251b9a9d644a39
dongjin <doshim1234@naver.com>
2025-02-11 12:37:57 +0900

    크싸 필터링 수정

--- src/main/java/com/skrookies/dahaezlge/controller/qna/QnaController.java
+++ src/main/java/com/skrookies/dahaezlge/controller/qna/QnaController.java
@@ -158,13 +158,13 @@ public class QnaController {
 
 
 
-//        // SQL, XSS 필터링 적용
-//        String qna_title = xssFilterService.filter(qnaDto.getQna_title());
-//        qnaDto.setQna_title(sqlFilterService.filter(qna_title));
-//
-//        /** XSS 공격 허용 */
-//        String qna_body = xssFilterService.filter1(qnaDto.getQna_body());
-//        qnaDto.setQna_body(sqlFilterService.filter2(qna_body));
+        // SQL, XSS 필터링 적용
+        String qna_title = xssFilterService.filter(qnaDto.getQna_title());
+        qnaDto.setQna_title(sqlFilterService.filter(qna_title));
+
+        /** XSS 공격 허용 */
+        String qna_body = xssFilterService.filter1(qnaDto.getQna_body());
+        qnaDto.setQna_body(sqlFilterService.filter2(qna_body));
 
         // 제목과 내용이 비어 있는 경우 예외 처리
         if (qnaDto.getQna_title() == null || qnaDto.getQna_title().trim().isEmpty()) {
@@ -289,13 +289,13 @@ public class QnaController {
             return "qnaWrite";
         }
 
-//        // SQL, XSS 필터링 적용
-//        String qna_title = xssFilterService.filter(QnaDto.getQna_title());
-//        QnaDto.setQna_title(sqlFilterService.filter(qna_title));
-//
-//        /** XSS 공격 허용 */
-//        String qna_body = xssFilterService.filter1(QnaDto.getQna_body());
-//        QnaDto.setQna_body(sqlFilterService.filter2(qna_body));
+        // SQL, XSS 필터링 적용
+        String qna_title = xssFilterService.filter(QnaDto.getQna_title());
+        QnaDto.setQna_title(sqlFilterService.filter(qna_title));
+
+        /** XSS 공격 허용 */
+        String qna_body = xssFilterService.filter1(QnaDto.getQna_body());
+        QnaDto.setQna_body(sqlFilterService.filter2(qna_body));
 
         // 필터링 되어 제목과 내용이 비어 있는 경우 예외 처리
         if (QnaDto.getQna_title() == null || QnaDto.getQna_title().trim().isEmpty()) {
