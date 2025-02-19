4cf39d75b032d06d95d8a127dc2115be9256dfae
heavenring <anstn0104@naver.com>
2025-02-20 08:52:38 +0900

    fix: CVE 포함

--- src/main/java/com/skrookies/dahaezlge/DahaezlgeApplication.java
+++ src/main/java/com/skrookies/dahaezlge/DahaezlgeApplication.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DahaezlgeApplication {
+public class DahaezlgeApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DahaezlgeApplication.class);
+    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(DahaezlgeApplication.class)
